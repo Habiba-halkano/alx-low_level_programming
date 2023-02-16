aad7960aa614ebd9fb80fa345a2ce4d101e6bbfb
Habiba <habzhalka@gmail.com>
2023-02-16 03:25:17 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints a string using fput
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
