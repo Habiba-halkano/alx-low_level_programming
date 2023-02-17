79f57ae80d389f0fc9c4401ade8ec2be4534ca6a
Habiba <habzhalka@gmail.com>
2023-02-17 01:08:31 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lower case
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+char c;
+
+c = a;
+while (c <= 'z'){
+	putchar(c);
+	c++;
+}
+putchar("\n");
+return (0);
+}
