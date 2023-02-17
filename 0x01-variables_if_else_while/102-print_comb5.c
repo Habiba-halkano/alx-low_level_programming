1b8331fb7016eccfec20ca744f9ab37c4a5a6dc3
Habiba <habzhalka@gmail.com>
2023-02-17 06:57:08 -0800

    primt comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,13 +7,13 @@ int main(void)
 {
 int a, b, c, d;
 
-for (a = '0'; a <= '9'; a++)
+for (a = 0; a <= 9; a++)
 {
-	for (b = '0'; b <= '9'; b++)
+	for (b = 0; b <= 9; b++)
 	{
-		for (c = '0'; c <= '9'; c++)
+		for (c = 0; c <= 9; c++)
 		{
-			for (d = '0'; d <= '9'; d++)
+			for (d = 0; d <= 9; d++)
 			{
 				if (((c + d) > (a + b) &&  c >= a) || a < c)
 				{
