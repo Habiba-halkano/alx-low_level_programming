d84cc479607d66e14b4856ea61a23a01a88d15d9
Habiba <habzhalka@gmail.com>
2023-02-22 12:42:08 -0800

    times table

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: print from this number
+ */
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		if (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				_putchar("%d", i)
+			else if(i == 98)
+				_putchar("d\n",i)
+			}
+		}
+		else if (n <= 98)
+		{
+			for (j = n; j >= 98; j--)
+			{
+				if (j != 98)
+					
