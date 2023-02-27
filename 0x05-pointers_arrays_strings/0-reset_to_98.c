01ae3d9a4161950a273d6e6d33033b55ea39d7c2
Habiba <habzhalka@gmail.com>
2023-02-27 06:11:02 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a pointer as a parameter
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this project consists of tasks on pointers, arrays and strings
