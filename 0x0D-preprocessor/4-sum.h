2bf5ba31890d55f4386662e6d42b76eba8d1be9c
Habiba <habzhalka@gmail.com>
2023-03-20 08:14:04 -0700

    macro SUM(x, y)

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * desc - function-like macro that computes the sum of
+ * x and y
+ * @x: one of the two numbers used
+ * @y: number used
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
