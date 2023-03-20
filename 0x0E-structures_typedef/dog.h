61caa5931aa8fdd47269ab62c8730aa37c584c69
Habiba <habzhalka@gmail.com>
2023-03-20 08:32:16 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef __DOG_H_
+#define __DOG_H_
+
+/**
+ * struct dog - structure of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
